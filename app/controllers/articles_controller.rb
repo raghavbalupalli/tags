476c5559e31f8ba9ecb79a8e8fdfb3a2cf86bed9
raghavbalupalli <raghavbalupalli@gmail.com>
2016-07-20 15:58:21 -0700

    tag links and tag cloud added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,11 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
+    if params[:tag]
+    @articles = Article.tagged_with(params[:tag])
+  else
     @articles = Article.all
+  end
   end
 
   # GET /articles/1
@@ -69,6 +73,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:name, :description)
+      params.require(:article).permit(:name, :description,:tag_list)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
+   get 'tags/:tag', to: 'articles#index', as: :tag
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
